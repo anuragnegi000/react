9adeab1f5da2a19789f271a8e83a0db9a7b622cc
anuragnegi000 <anurag.negi862@gmail.com>
2024-01-16 18:40:35 +0530

    Card wrapper basic

---
+++ App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+
+
+
+function App() { 
+
+  return (
+   <div>  
+    <CardWrapper>
+      hi there
+    </CardWrapper>
+   </div>   
+  )
+}
+
+function CardWrapper({children}){ 
+  return <div style={{border:"2px solid black",padding:20}}>
+    {children}
+  </div>
+}
+
+
+export default App
+ 
